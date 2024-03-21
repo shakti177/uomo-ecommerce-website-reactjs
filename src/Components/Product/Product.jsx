64a321dc3e6013e6e7859d70d66a605c6326e17f
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-03-22 00:24:08 +0530

    "Add product details section with quantity control, wishlist, and share functionality; update product gallery and thumbnail styling"

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -7,10 +7,17 @@ import product4 from "../../Assets/ProductDetail/productdetail-4.jpg";
 
 import { GoChevronLeft } from "react-icons/go";
 import { GoChevronRight } from "react-icons/go";
+import { FaStar } from "react-icons/fa";
+import { FiHeart } from "react-icons/fi";
+import { PiShareNetworkLight } from "react-icons/pi";
+
+import { Link } from "react-router-dom";
 
 import "./Product.css";
 
 const Product = () => {
+  // Product images Gallery
+
   const productImg = [product1, product2, product3, product4];
   const [currentImg, setCurrentImg] = useState(0);
 
@@ -22,29 +29,136 @@ const Product = () => {
     setCurrentImg(currentImg === productImg.length - 1 ? 0 : currentImg + 1);
   };
 
+  // Product Quantity
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value) && value > 0) {
+      setQuantity(value);
+    }
+  };
+
+  // Product WishList
+
+  const [clicked, setClicked] = useState(false);
+
+  const handleWishClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <div>
       <div className="productSection">
-        <div className="productGallery">
-          <div className="productThumb">
-            <img src={product1} onClick={() => setCurrentImg(0)} alt="" />
-            <img src={product2} onClick={() => setCurrentImg(1)} alt="" />
-            <img src={product3} onClick={() => setCurrentImg(2)} alt="" />
-            <img src={product4} onClick={() => setCurrentImg(3)} alt="" />
+        <div className="productShowCase">
+          <div className="productGallery">
+            <div className="productThumb">
+              <img src={product1} onClick={() => setCurrentImg(0)} alt="" />
+              <img src={product2} onClick={() => setCurrentImg(1)} alt="" />
+              <img src={product3} onClick={() => setCurrentImg(2)} alt="" />
+              <img src={product4} onClick={() => setCurrentImg(3)} alt="" />
+            </div>
+            <div className="productFullImg">
+              <img src={productImg[currentImg]} alt="" />
+              <div className="buttonsGroup">
+                <button onClick={prevImg} className="directionBtn">
+                  <GoChevronLeft size={18} />
+                </button>
+                <button onClick={nextImg} className="directionBtn">
+                  <GoChevronRight size={18} />
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="productFullImg">
-            <img src={productImg[currentImg]} alt="" />
-            <div className="buttonsGroup">
-              <button onClick={prevImg} className="directionBtn">
-                <GoChevronLeft size={18} />
-              </button>
-              <button onClick={nextImg} className="directionBtn">
-                <GoChevronRight size={18} />
-              </button>
+          <div className="productDetails">
+            <div className="productBreadcrumb">
+              <div className="breadcrumbLink">
+                <Link to="/">Home</Link>&nbsp;/&nbsp;
+                <Link to="/shop">The Shop</Link>
+              </div>
+              <div className="prevNextLink">
+                <Link to="/product">
+                  <GoChevronLeft />
+                  <p>Prev</p>
+                </Link>
+                <Link to="/product">
+                  <p>Next</p>
+                  <GoChevronRight />
+                </Link>
+              </div>
+            </div>
+            <div className="productName">
+              <h1>Lightweight Puffer Jacket With a Hood</h1>
+            </div>
+            <div className="productRating">
+              <FaStar color="#FEC78A" size={10} />
+              <FaStar color="#FEC78A" size={10} />
+              <FaStar color="#FEC78A" size={10} />
+              <FaStar color="#FEC78A" size={10} />
+              <FaStar color="#FEC78A" size={10} />
+              <p>8k+ reviews</p>
+            </div>
+            <div className="productPrice">
+              <h3>$449</h3>
+            </div>
+            <div className="productDescription">
+              <p>
+                Phasellus sed volutpat orci. Fusce eget lore mauris vehicula
+                elementum gravida nec dui. Aenean aliquam varius ipsum, non
+                ultricies tellus sodales eu. Donec dignissim viverra nunc, ut
+                aliquet magna posuere eget.
+              </p>
+            </div>
+            <div className="productCartQuantity">
+              <div className="productQuantity">
+                <button onClick={decrement}>-</button>
+                <input
+                  type="text"
+                  value={quantity}
+                  onChange={handleInputChange}
+                />
+                <button onClick={increment}>+</button>
+              </div>
+              <div className="productCartBtn">
+                <button>Add to Cart</button>
+              </div>
+            </div>
+            <div className="productWishShare">
+              <div className="productWishList">
+                <button onClick={handleWishClick}>
+                  <FiHeart color={clicked ? "red" : ""} size={17} />
+                  <p>Add to Wishlist</p>
+                </button>
+              </div>
+              <div className="productShare">
+                <PiShareNetworkLight size={22} />
+                <p>Share</p>
+              </div>
+            </div>
+            <div className="productTags">
+              <p>
+                <span>SKU: </span>N/A
+              </p>
+              <p>
+                <span>CATEGORIES: </span>Casual & Urban Wear, Jackets, Men
+              </p>
+              <p>
+                <span>TAGS: </span>biker, black, bomber, leather
+              </p>
             </div>
           </div>
         </div>
-        <div className="productDetails"></div>
       </div>
     </div>
   );
