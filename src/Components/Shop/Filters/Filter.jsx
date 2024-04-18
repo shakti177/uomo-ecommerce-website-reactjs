bd2f07d8477c05f2d937515b455168bedaea6e48
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-04-18 23:58:38 +0530

    "Add price range slider to Filter component"

--- src/Components/Shop/Filters/Filter.jsx
+++ src/Components/Shop/Filters/Filter.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Filter.css";
 
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import { IoIosArrowDown } from "react-icons/io";
+import Slider from "@mui/material/Slider";
 
 const Filter = () => {
+  function valuetext(value) {
+    return `$${value}`;
+  }
+
+  const [value, setValue] = useState([20, 37]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       <div className="filterSection">
@@ -89,9 +100,16 @@ const Filter = () => {
               <h5 className="filterHeading">Price</h5>
             </AccordionSummary>
             <AccordionDetails sx={{ padding: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              <Slider
+                getAriaLabel={() => "Temperature range"}
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+                sx={{
+                  color: "black",
+                }}
+              />
             </AccordionDetails>
           </Accordion>
         </div>
