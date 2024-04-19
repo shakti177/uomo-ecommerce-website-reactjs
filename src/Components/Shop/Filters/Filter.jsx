5a1212a102c0fdc522383f7e33e3b314f58f667d
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-04-19 15:20:07 +0530

    "Add filtering functionality for shop: color, size, brand, and price range"

--- src/Components/Shop/Filters/Filter.jsx
+++ src/Components/Shop/Filters/Filter.jsx
@@ -5,14 +5,76 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import { IoIosArrowDown } from "react-icons/io";
+import { BiSearch } from "react-icons/bi";
 import Slider from "@mui/material/Slider";
 
 const Filter = () => {
   const [value, setValue] = useState([20, 69]);
 
+  const [selectedColors, setSelectedColors] = useState([]);
+  const [selectedSizes, setSelectedSizes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [brandsData] = useState([
+    { name: "Adidas", count: 2 },
+    { name: "Balmain", count: 7 },
+    { name: "Balenciaga", count: 10 },
+    { name: "Burberry", count: 39 },
+    { name: "Kenzo", count: 95 },
+    { name: "Givenchy", count: 1092 },
+    { name: "Zara", count: 48 },
+  ]);
+
+  const handleColorChange = (color) => {
+    setSelectedColors((prevColors) =>
+      prevColors.includes(color)
+        ? prevColors.filter((c) => c !== color)
+        : [...prevColors, color]
+    );
+  };
+
+  const handleSizeChange = (size) => {
+    setSelectedSizes((prevSizes) =>
+      prevSizes.includes(size)
+        ? prevSizes.filter((s) => s !== size)
+        : [...prevSizes, size]
+    );
+  };
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const filteredBrands = brandsData.filter((brand) =>
+    brand.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const filterCategories = [
+    "Dresses",
+    "Shorts",
+    "Sweatshirts",
+    "Swimwear",
+    "Jackets",
+    "T-Shirts & Tops",
+    "Jeans",
+    "Trousers",
+    "Men",
+    "Jumpers & Cardigans",
+  ];
+
+  const filterColors = [
+    "#0B2472",
+    "#D6BB4F",
+    "#282828",
+    "#B0D6E8",
+    "#9C7539",
+    "#D29B47",
+    "#E5AE95",
+    "#D76B67",
+    "#BABABA",
+    "#BFDCC4",
+  ];
+
+  const filterSizes = ["XS", "S", "M", "L", "XL", "XXL"];
 
   return (
     <div>
@@ -23,14 +85,14 @@ const Filter = () => {
               expandIcon={<IoIosArrowDown size={20} />}
               aria-controls="panel1-content"
               id="panel1-header"
-              sx={{ padding: 0 }}
+              sx={{ padding: 0, marginBottom: 2 }}
             >
               <h5 className="filterHeading">Product Categories</h5>
             </AccordionSummary>
             <AccordionDetails sx={{ padding: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              {filterCategories.map((category, index) => (
+                <p key={index}>{category}</p>
+              ))}
             </AccordionDetails>
           </Accordion>
         </div>
@@ -40,14 +102,27 @@ const Filter = () => {
               expandIcon={<IoIosArrowDown size={20} />}
               aria-controls="panel1-content"
               id="panel1-header"
-              sx={{ padding: 0 }}
+              sx={{ padding: 0, marginBottom: 2 }}
             >
               <h5 className="filterHeading">Color</h5>
             </AccordionSummary>
             <AccordionDetails sx={{ padding: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              {
+                <div className="filterColorBtn">
+                  {filterColors.map((color, index) => (
+                    <button
+                      key={index}
+                      className={`colorButton ${
+                        selectedColors.includes(color) ? "selected" : ""
+                      }`}
+                      style={{
+                        backgroundColor: color,
+                      }}
+                      onClick={() => handleColorChange(color)}
+                    />
+                  ))}
+                </div>
+              }
             </AccordionDetails>
           </Accordion>
         </div>
@@ -57,14 +132,24 @@ const Filter = () => {
               expandIcon={<IoIosArrowDown size={20} />}
               aria-controls="panel1-content"
               id="panel1-header"
-              sx={{ padding: 0 }}
+              sx={{ padding: 0, marginBottom: 2 }}
             >
               <h5 className="filterHeading">Sizes</h5>
             </AccordionSummary>
             <AccordionDetails sx={{ padding: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              <div className="sizeButtons">
+                {filterSizes.map((size, index) => (
+                  <button
+                    key={index}
+                    className={`sizeButton ${
+                      selectedSizes.includes(size) ? "selected" : ""
+                    }`}
+                    onClick={() => handleSizeChange(size)}
+                  >
+                    {size}
+                  </button>
+                ))}
+              </div>
             </AccordionDetails>
           </Accordion>
         </div>
@@ -74,14 +159,46 @@ const Filter = () => {
               expandIcon={<IoIosArrowDown size={20} />}
               aria-controls="panel1-content"
               id="panel1-header"
-              sx={{ padding: 0 }}
+              sx={{ padding: 0, marginBottom: 2 }}
             >
               <h5 className="filterHeading">Brands</h5>
             </AccordionSummary>
             <AccordionDetails sx={{ padding: 0 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+              {/* Search bar */}
+              <div className="searchBar">
+                <BiSearch className="searchIcon" size={20} color={"#767676"} />
+                <input
+                  type="text"
+                  placeholder="Search"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+
+              {/* Brand list */}
+              <div className="brandList">
+                {filteredBrands.length > 0 ? (
+                  filteredBrands.map((brand, index) => (
+                    <div className="brandItem" key={index}>
+                      {/* Radio button */}
+                      <input
+                        type="checkbox"
+                        name="brand"
+                        id={`brand-${index}`}
+                        className="brandRadio"
+                      />
+                      {/* Brand name */}
+                      <label htmlFor={`brand-${index}`} className="brandLabel">
+                        {brand.name}
+                      </label>
+                      {/* Brand count */}
+                      <span className="brandCount">{brand.count}</span>
+                    </div>
+                  ))
+                ) : (
+                  <div className="notFoundMessage">Not found</div>
+                )}
+              </div>
             </AccordionDetails>
           </Accordion>
         </div>
@@ -91,7 +208,7 @@ const Filter = () => {
               expandIcon={<IoIosArrowDown size={20} />}
               aria-controls="panel1-content"
               id="panel1-header"
-              sx={{ padding: 0 }}
+              sx={{ padding: 0, marginBottom: 2 }}
             >
               <h5 className="filterHeading">Price</h5>
             </AccordionSummary>
@@ -104,8 +221,25 @@ const Filter = () => {
                 valueLabelFormat={(value) => `$${value}`}
                 sx={{
                   color: "black",
+                  "& .MuiSlider-thumb": {
+                    backgroundColor: "white",
+                    border: "2px solid black",
+                    width: 18,
+                    height: 18,
+                  },
                 }}
               />
+
+              <div className="filterSliderPrice">
+                <div className="priceRange">
+                  <p>
+                    Min Price: <span>${value[0]}</span>
+                  </p>
+                  <p>
+                    Max Price: <span>${value[1]}</span>
+                  </p>
+                </div>
+              </div>
             </AccordionDetails>
           </Accordion>
         </div>
