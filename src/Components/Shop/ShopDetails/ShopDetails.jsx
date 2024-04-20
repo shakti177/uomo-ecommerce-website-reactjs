ad0f856c1ba1ff8bfa1897eaba10afdd58a022fc
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-04-20 23:25:33 +0530

    Add shop details right section with breadcrumb and sorting functionality

--- src/Components/Shop/ShopDetails/ShopDetails.jsx
+++ src/Components/Shop/ShopDetails/ShopDetails.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Filter from "../Filters/Filter";
 
+import { Link } from "react-router-dom";
+
 import "./ShopDetails.css";
 
 const ShopDetails = () => {
@@ -11,7 +13,29 @@ const ShopDetails = () => {
           <div className="shopDetails__left">
             <Filter />
           </div>
-          <div className="shopDetails__right">Products List</div>
+          <div className="shopDetails__right">
+            <div className="shopDetailsSorting">
+              <div className="shopDetailsBreadcrumbLink">
+                <Link to="/">Home</Link>&nbsp;/&nbsp;
+                <Link to="/shop">The Shop</Link>
+              </div>
+              <div className="shopDetailsSort">
+                <select name="sort" id="sort">
+                  <option value="default">Default Sorting</option>
+                  <option value="Featured">Featured</option>
+                  <option value="bestSelling">Best Selling</option>
+                  <option value="a-z">Alphabetically, A-Z</option>
+                  <option value="z-a">Alphabetically, Z-A</option>
+                  <option value="lowToHigh">Price, Low to high</option>
+                  <option value="highToLow">Price, high to low</option>
+                  <option value="oldToNew">Date, old to new</option>
+                  <option value="newToOld">Date, new to old</option>
+                </select>
+              </div>
+            </div>
+            <div className="shopDetailsProducts"></div>
+            <div className="shopDetailsPagination"></div>
+          </div>
         </div>
       </div>
     </div>
