f48600c62a95dd66e8b086ec8ebeac749f539c42
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-04-26 23:40:45 +0530

    "Added wishlist functionality to ShopDetails component and updated CSS"

--- src/Components/Shop/ShopDetails/ShopDetails.jsx
+++ src/Components/Shop/ShopDetails/ShopDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "../Filters/Filter";
 
 import { Link } from "react-router-dom";
@@ -11,7 +11,19 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 
+import { FiHeart } from "react-icons/fi";
+import { FaStar } from "react-icons/fa";
+
 const ShopDetails = () => {
+  const [wishList, setWishList] = useState({});
+
+  const handleWishlistClick = (productID) => {
+    setWishList((prevWishlist) => ({
+      ...prevWishlist,
+      [productID]: !prevWishlist[productID],
+    }));
+  };
+
   return (
     <div>
       <div className="shopDetails">
@@ -40,16 +52,51 @@ const ShopDetails = () => {
               </div>
             </div>
             <div className="shopDetailsProducts">
-              {StoreData.map((product) => (
+              {StoreData.slice(0, 7).map((product) => (
                 <div className="shopDetailsProduct" key={product.id}>
-                  <Swiper navigation={true} modules={[Navigation]} loop={true}>
-                    <SwiperSlide>
-                      <img src={product.frontImg} alt="" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <img src={product.backImg} alt="" />
-                    </SwiperSlide>
-                  </Swiper>
+                  <div className="shopDetailsProductImages">
+                    <Swiper
+                      navigation={true}
+                      modules={[Navigation]}
+                      loop={true}
+                    >
+                      <SwiperSlide>
+                        <img src={product.frontImg} alt="" />
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <img src={product.backImg} alt="" />
+                      </SwiperSlide>
+                    </Swiper>
+                  </div>
+
+                  <div className="shopDetailsProductInfo">
+                    <div className="sdCategoryWishList">
+                      <p>Dresses</p>
+                      <FiHeart
+                        onClick={() => handleWishlistClick(product.productID)}
+                        style={{
+                          color: wishList[product.productID]
+                            ? "red"
+                            : "#767676",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </div>
+                    <div className="sdProductNameInfo">
+                      <h5>{product.productName}</h5>
+                      <p>{product.productPrice}</p>
+                      <div className="sdProductRatingReviews">
+                        <div className="sdProductRating">
+                          <FaStar color="#FEC78A" size={10} />
+                          <FaStar color="#FEC78A" size={10} />
+                          <FaStar color="#FEC78A" size={10} />
+                          <FaStar color="#FEC78A" size={10} />
+                          <FaStar color="#FEC78A" size={10} />
+                        </div>
+                        <span>{product.productReviews}</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
