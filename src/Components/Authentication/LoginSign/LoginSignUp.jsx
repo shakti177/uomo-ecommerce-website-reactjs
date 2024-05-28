55efda0bc40147a2dab4de1f69c632281307eaaa
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-05-28 15:45:01 +0530

    "Added CSS styles and implemented login/register tab functionality in LoginSignUp component"

--- src/Components/Authentication/LoginSign/LoginSignUp.jsx
+++ src/Components/Authentication/LoginSign/LoginSignUp.jsx
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginSignUp.css";
 
 const LoginSignUp = () => {
-  return <div>Login</div>;
+  const [activeTab, setActiveTab] = useState("tabButton1");
+
+  const handleTab = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <div className="loginSignUpSection">
+        <div className="loginSignUpContainer">
+          <div className="loginSignUpTabs">
+            <p
+              onClick={() => handleTab("tabButton1")}
+              className={activeTab === "tabButton1" ? "active" : ""}
+            >
+              Login
+            </p>
+            <p
+              onClick={() => handleTab("tabButton2")}
+              className={activeTab === "tabButton2" ? "active" : ""}
+            >
+              Register
+            </p>
+          </div>
+          <div className="loginSignUpTabsContent">
+            {/* tab1 */}
+
+            {activeTab === "tabButton1" && (
+              <div className="loginSignUpTabsContentLogin">
+                <form>
+                  <input type="text" placeholder="Email address *" required />
+                  <input type="password" placeholder="Password *" required />
+                  <div className="loginSignUpForgetPass">
+                    <label>
+                      <input type="checkbox" className="brandRadio" />
+                      <p>Remember me</p>
+                    </label>
+                    <p style={{ textDecoration: "underline", color: "black" }}>
+                      Lost password?
+                    </p>
+                  </div>
+                  <button>Log In</button>
+                </form>
+                <div className="loginSignUpTabsContentLoginText">
+                  <p>
+                    No account yet?{" "}
+                    <span onClick={() => handleTab("tabButton2")}>
+                      Create Account
+                    </span>
+                  </p>
+                </div>
+              </div>
+            )}
+
+            {/* Tab2 */}
+
+            {activeTab === "tabButton2" && (
+              <div className="loginSignUpTabsContentRegister">
+                <form>
+                  <input type="text" placeholder="Username *" required />
+                  <input type="text" placeholder="Email address *" required />
+                  <input type="password" placeholder="Password *" required />
+                  <p>
+                    Your personal data will be used to support your experience
+                    throughout this website, to manage access to your account,
+                    and for other purposes described in our privacy policy.
+                  </p>
+                  <button>Register</button>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignUp;
