c9c1db20147131dcd432b4fc71088db1ea974d09
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-05-28 16:08:25 +0530

    "Added CSS styles and JSX code for Reset Password component"

--- src/Components/Authentication/Reset/ResetPass.jsx
+++ src/Components/Authentication/Reset/ResetPass.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import "./ResetPass.css";
+import { Link } from "react-router-dom";
+
 const ResetPass = () => {
-  return <div>ResetPassword</div>;
+  return (
+    <div>
+      <div className="resetPasswordSection">
+        <h2>Reset Your Password</h2>
+        <div className="resetPasswordContainer">
+          <p>We will send you an email to reset your password</p>
+          <form>
+            <input type="email" placeholder="Email address *" required />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <p>
+          Back to{" "}
+          <Link to="/loginSignUp">
+            <span>Login</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ResetPass;
