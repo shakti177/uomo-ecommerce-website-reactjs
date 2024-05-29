059cdd11324e356c19dc0fa291a3df85c649e576
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-05-29 11:57:10 +0530

    "Updated Navbar CSS and JSX: added search bar, language and currency selectors, social links, and refactored mobile menu layout and styles."

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -8,8 +8,16 @@ import { FiSearch } from "react-icons/fi";
 import { FaRegUser } from "react-icons/fa6";
 import { RiShoppingBagLine } from "react-icons/ri";
 import { MdOutlineClose } from "react-icons/md";
-
 import { FiHeart } from "react-icons/fi";
+
+// social Links imports Icons
+
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+
 import Badge from "@mui/material/Badge";
 
 const Navbar = () => {
@@ -95,38 +103,78 @@ const Navbar = () => {
           </Badge>
         </div>
         <div className={`mobile-menu ${mobileMenuOpen ? "open" : ""}`}>
-          <ul>
-            <li>
-              <Link to="/" onClick={toggleMobileMenu}>
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to="/shop" onClick={toggleMobileMenu}>
-                SHOP
-              </Link>
-            </li>
-            <li>
-              <Link to="/blog" onClick={toggleMobileMenu}>
-                BLOG
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={toggleMobileMenu}>
-                ABOUT
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" onClick={toggleMobileMenu}>
-                CONTACT
-              </Link>
-            </li>
-          </ul>
+          <div className="mobile-menuTop">
+            <div className="mobile-menuSearchBar">
+              <div className="mobile-menuSearchBarContainer">
+                <input type="text" placeholder="Search products" />
+                <FiSearch size={22} />
+              </div>
+            </div>
+            <div className="mobile-menuList">
+              <ul>
+                <li>
+                  <Link to="/" onClick={toggleMobileMenu}>
+                    HOME
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/shop" onClick={toggleMobileMenu}>
+                    SHOP
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/blog" onClick={toggleMobileMenu}>
+                    BLOG
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" onClick={toggleMobileMenu}>
+                    ABOUT
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact" onClick={toggleMobileMenu}>
+                    CONTACT
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
           <div className="mobile-menuFooter">
-            <Link to="/loginSignUp" onClick={toggleMobileMenu}>
-              <FaRegUser />
-              <p>My Account</p>
-            </Link>
+            <div className="mobile-menuFooterLogin">
+              <Link to="/loginSignUp" onClick={toggleMobileMenu}>
+                <FaRegUser />
+                <p>My Account</p>
+              </Link>
+            </div>
+            <div className="mobile-menuFooterLangCurrency">
+              <div className="mobile-menuFooterLang">
+                <p>Language</p>
+                <select name="language" id="language">
+                  <option value="english">United States | English</option>
+                  <option value="Hindi">Hindi</option>
+                  <option value="Germany">Germany</option>
+                  <option value="French">French</option>
+                </select>
+              </div>
+              <div className="mobile-menuFooterCurrency">
+                <p>Currency</p>
+                <select name="currency" id="currency">
+                  <option value="USD">$ USD</option>
+                  <option value="INR">₹ INR</option>
+                  <option value="EUR">€ EUR</option>
+                  <option value="GBP">£ GBP</option>
+                </select>
+              </div>
+            </div>
+            <div className="mobile-menuSocial_links">
+              <FaFacebookF />
+              <FaXTwitter />
+              <FaInstagram />
+              <FaYoutube />
+              <FaPinterest />
+            </div>
           </div>
         </div>
       </nav>
