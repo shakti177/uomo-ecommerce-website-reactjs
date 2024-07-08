2441adea8197796d1b656d9d9616fd8252476173
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-07-09 00:16:40 +0530

    Added "loading='lazy'" attribute to two img tags in Trendy.jsx component.

--- src/Components/Home/Trendy/Trendy.jsx
+++ src/Components/Home/Trendy/Trendy.jsx
@@ -97,11 +97,13 @@ const Trendy = () => {
                             src={product.frontImg}
                             alt=""
                             className="trendyProduct_front"
+                            loading="lazy"
                           />
                           <img
                             src={product.backImg}
                             alt=""
                             className="trendyProduct_back"
+                            loading="lazy"
                           />
                         </Link>
                         <h4 onClick={() => dispatch(addToCart(product))}>
