beda7f7dc9abbc63ac716148da98dfd9a467e648
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-09-04 12:38:02 +0530

    "Uncommented HeroSection import and added it to Home component"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,12 @@ import Instagram from "../Components/Home/Instagram/Instagram";
 import Trendy from "../Components/Home/Trendy/Trendy";
 import LimitedEdition from "../Components/Home/Limited/LimitedEdition";
 import DealTimer from "../Components/Home/Deal/DealTimer";
-// import HeroSection from "../Components/Home/Hero/HeroSection";
+import HeroSection from "../Components/Home/Hero/HeroSection";
 
 const Home = () => {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       <CollectionBox />
       <Trendy />
       <DealTimer />
