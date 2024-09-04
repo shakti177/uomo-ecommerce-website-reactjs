1a3a1f760762770ea7dc02a462e01f13e7a58bbf
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2024-09-04 12:34:13 +0530

    Hero Section Removed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ import Instagram from "../Components/Home/Instagram/Instagram";
 import Trendy from "../Components/Home/Trendy/Trendy";
 import LimitedEdition from "../Components/Home/Limited/LimitedEdition";
 import DealTimer from "../Components/Home/Deal/DealTimer";
-import HeroSection from "../Components/Home/Hero/HeroSection";
+// import HeroSection from "../Components/Home/Hero/HeroSection";
 
 const Home = () => {
   return (
